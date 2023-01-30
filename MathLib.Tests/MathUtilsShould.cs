14151540f3fb0ba30fc41dae8ca0a5f54cffae61
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-30 18:39:43 +0530

    done

--- MathLib.Tests/MathUtilsShould.cs
+++ MathLib.Tests/MathUtilsShould.cs
@@ -3,12 +3,8 @@ namespace MathLib.Tests
     public class MathUtilsShould
     {
         [Theory]
-        //  We use MemberData attribute to provide external data
-        //  prefer nameof() operator to hardcoded string values
-        //  The static method can reside any class, so long as it is public
-        
-        [MemberData(nameof(GetTestDataLocal))]
-        //  [MemberData(nameof(DataProvider.GetTestDataExternal))]    //  also works
+        //  If a static method in another method is referenced to MemberData use this overload
+        [MemberData(nameof(DataProvider.GetTestDataExternal), MemberType = typeof(DataProvider))]
         public void CalculateProductOfIntegers(int op1, int op2, int product)
         {
             //  Arrange
@@ -20,33 +16,17 @@ namespace MathLib.Tests
             //  Assert
             Assert.Equal(product, result);
         }
-
-        //  a public static method that can return IEnumerable<object[]> can provide the data to the theory
-        public static IEnumerable<object[]> GetTestDataLocal()
-        {
-            //  compiler builds a state machine (iterator) when you use yield return 
-            //  more info: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
-            //  https://stackoverflow.blog/2022/06/15/c-ienumerable-yield-return-and-lazy-evaluation/
-            yield return new object[] { 5, 10, 50 };
-            yield return new object[] { -7, 7, -49 };
-            yield return new object[] { -5, 0, 0 };
-            yield return new object[] { 5, 0, 0 };
-            yield return new object[] { 0, 0, 0 };
-        }
     }
 
-    public class DataProvider
+    public static class DataProvider
     {
         public static IEnumerable<object[]> GetTestDataExternal()
         {
-            //  compiler builds a state machine (iterator) when you use yield return 
-            //  more info: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
-            //  https://stackoverflow.blog/2022/06/15/c-ienumerable-yield-return-and-lazy-evaluation/
-            yield return new object[] { 5, 10, 50 };
-            yield return new object[] { -7, 7, -49 };
-            yield return new object[] { -5, 0, 0 };
-            yield return new object[] { 5, 0, 0 };
-            yield return new object[] { 0, 0, 0 };
+            //  Make sure the csv file's Copy to Output Directory property is set to Copy Always
+            var lines = File.ReadAllLines("test-data.csv");
+            return lines
+                .Select(l => l.Split(','))
+                .Select(sa => sa.Select(s => Convert.ToInt32(s.Trim()) as object).ToArray());
         }
     }
 }
