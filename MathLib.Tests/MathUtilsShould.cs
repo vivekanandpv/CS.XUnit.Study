dfd785d360738fc37918c227343e80c558016ab2
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-30 18:18:18 +0530

    done

--- MathLib.Tests/MathUtilsShould.cs
+++ MathLib.Tests/MathUtilsShould.cs
@@ -3,11 +3,12 @@ namespace MathLib.Tests
     public class MathUtilsShould
     {
         [Theory]
-        [InlineData(5, 10, 50)]
-        [InlineData(-7, 7, -49)]
-        [InlineData(-5, 0, 0)]
-        [InlineData(5, 0, 0)]
-        [InlineData(0, 0, 0)]
+        //  We use MemberData attribute to provide external data
+        //  prefer nameof() operator to hardcoded string values
+        //  The static method can reside any class, so long as it is public
+        
+        [MemberData(nameof(GetTestDataLocal))]
+        //  [MemberData(nameof(DataProvider.GetTestDataExternal))]    //  also works
         public void CalculateProductOfIntegers(int op1, int op2, int product)
         {
             //  Arrange
@@ -18,6 +19,34 @@ namespace MathLib.Tests
 
             //  Assert
             Assert.Equal(product, result);
+        }
+
+        //  a public static method that can return IEnumerable<object[]> can provide the data to the theory
+        public static IEnumerable<object[]> GetTestDataLocal()
+        {
+            //  compiler builds a state machine (iterator) when you use yield return 
+            //  more info: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
+            //  https://stackoverflow.blog/2022/06/15/c-ienumerable-yield-return-and-lazy-evaluation/
+            yield return new object[] { 5, 10, 50 };
+            yield return new object[] { -7, 7, -49 };
+            yield return new object[] { -5, 0, 0 };
+            yield return new object[] { 5, 0, 0 };
+            yield return new object[] { 0, 0, 0 };
+        }
+    }
+
+    public class DataProvider
+    {
+        public static IEnumerable<object[]> GetTestDataExternal()
+        {
+            //  compiler builds a state machine (iterator) when you use yield return 
+            //  more info: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
+            //  https://stackoverflow.blog/2022/06/15/c-ienumerable-yield-return-and-lazy-evaluation/
+            yield return new object[] { 5, 10, 50 };
+            yield return new object[] { -7, 7, -49 };
+            yield return new object[] { -5, 0, 0 };
+            yield return new object[] { 5, 0, 0 };
+            yield return new object[] { 0, 0, 0 };
         }
     }
 }
