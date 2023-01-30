4d8775af169bbed24934873b1e704424e526a3b8
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-30 14:04:02 +0530

    done

--- MathLib.Tests/MathUtilsShould.cs
+++ MathLib.Tests/MathUtilsShould.cs
@@ -2,17 +2,22 @@ namespace MathLib.Tests
 {
     public class MathUtilsShould
     {
-        [Fact]
-        public void CalculateProductOfIntegers()
+        [Theory]
+        [InlineData(5, 10, 50)]
+        [InlineData(-7, 7, -49)]
+        [InlineData(-5, 0, 0)]
+        [InlineData(5, 0, 0)]
+        [InlineData(0, 0, 0)]
+        public void CalculateProductOfIntegers(int op1, int op2, int product)
         {
             //  Arrange
             var utils = new MathUtils();
 
             //  Act
-            var result = utils.GetProduct(5, 10);
+            var result = utils.GetProduct(op1, op2);
 
             //  Assert
-            Assert.Equal(50, result);
+            Assert.Equal(product, result);
         }
     }
 }
